974174e876669654e50e19ce585def30a9786930
Danh Phan <phandanh2112@gmail.com>
2026-02-14 18:39:52 +0700

    v1.3.0: add typecast for decay pointer; add check addr

--- pointer/main.c
+++ pointer/main.c
@@ -7,25 +7,28 @@ int main() {
     printf("Pointer to int:\n");
     int *p = a;
 
-    printf("Address of a[0]: %p\n", (void*)p);
+    printf("Address of a[0]: %p\n", (void*)a);          // x
+    printf("Address of a: %p\n", (void*)&a);            // x
+    printf("Address of a+1: %p\n", (void*)(a + 1));     // x + 4(sizeof(int))
+    printf("Address of &a+1: %p\n", (void*)(&a + 1));   // x + 20(sizeof(a)) = x + 5 * 4
 
-    printf("sizeof a[0]: %d\n", sizeof(a[0]));  //4
-    printf("sizeof a: %d\n", sizeof(a));        //20
-    printf("sizeof p*: %d\n", sizeof(p));       //8
+    // printf("sizeof a[0]: %d\n", sizeof(a[0]));  //4
+    // printf("sizeof a: %d\n", sizeof(a));        //20
+    // printf("sizeof p*: %d\n", sizeof(p));       //8
 
-    sizeOfArr(a);  //8 - treated as pointer
+    // sizeOfArr(a);  //8 - treated as pointer
 
-    printf("get value, pointer point to next element: %d\n", *p++);
-    printf("increment value, pointer point to same element: %d\n", ++*p); 
-    printf("pointer point to next element, get value: %d\n", *++p); 
+    // printf("get value, pointer point to next element: %d\n", *p++);
+    // printf("increment value, pointer point to same element: %d\n", ++*p); 
+    // printf("pointer point to next element, get value: %d\n", *++p); 
 
 
     
-    /* pointer to array */
-    printf("\nPointer to array:\n");
-    int (*pArr)[] = &a;  //pointer to array of 5 integers
-    printf("Address of pArr: %p\n", (void*)pArr);
-    printf("pArr[2] = %d\n", (*pArr)[2]);  //3
+    // /* pointer to array */
+    // printf("\nPointer to array:\n");
+    // int (*pArr)[] = &a;  //pointer to array of 5 integers
+    // printf("Address of pArr: %p\n", (void*)pArr);
+    // printf("pArr[2] = %d\n", (*pArr)[2]);  //3
 
     return 0;
 }
--- pointer/main.h
+++ pointer/main.h
@@ -3,5 +3,5 @@
 // array-to-pointer decay: when an array is passed to a function, it decays to a pointer to its first element.
 // = void sizeOfArr(int *a)
 void sizeOfArr(int a[]) {   // a is treated as a pointer
-    printf("Size of array passed as param func: %zu\n", sizeof(a));
+    printf("Size of array passed as param func: %zu\n", sizeof((int*)a));
 }
